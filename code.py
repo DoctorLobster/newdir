b74670a5614a53d74b71430d584af5c87e275ea9
Mark Kuiack <kuiack@Marks-MacBook-Pro-3.local>
2017-08-30 11:08:31 -0700

    Add documentation to copy.py

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 
 def do_something():
+    '''Prints the first 3 digits of pie'''
     something = 3.14
     print(something)# this will make it much easier in future problems to see that something is actually happening
